0cbcf813c9af18189668236dd4a5ee65e6cd0ccc
Ichinose Yoshihiro <maruisansmai@gmail.com>
2021-12-04 23:17:48 +0900

    fixup! ✨ feat: motionコマンドを追加

--- src/cli.ts
+++ src/cli.ts
@@ -1,22 +1,16 @@
-import { NOTION_ROOT_PARENT_ID } from "./constant.ts";
-import {
-  martian,
-  Kia,
-  Command,
-  colors,
-  HelpCommand,
-  CompletionsCommand,
-} from "./deps.ts";
-import { log } from "./utils/logger.ts";
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+import { colors, Command, CompletionsCommand, HelpCommand } from "./deps.ts";
+import { motion } from "./command/motion.ts";
 
 const name = "mikan";
 const description = `
 マークダウンファイルをNotionに送信し新しいページとして挿入します
 `;
 const version = "0.0.1";
+const example = {
+  title: "move your-knowledge to Notion",
+  contents: `${colors.bgBlack.bold("$ mikan move")}
+search md file from ${colors.bold.green("./md")} and call Notion API`,
+};
 
 await new Command()
   .stopEarly()
@@ -26,27 +20,10 @@ await new Command()
   .env("NOTION_API_KEY=<value:string>", "Notion API key")
   .env("NOTION_ROOT_PARENT_ID=<value:string>", "Notion page id")
   .env("NOTION_DATABASE_ID=<value:string>", "Notion database id")
-  .example(
-    `${name} motion`,
-    `search md file from ${colors.bold.green("./md")}
-    and call Notion API`
-  )
-  .arguments("<command> [subcommand] [option]")
-  .command("motion", "import md to Notion")
-  .action(async (options: any) => {
-    const kia = new Kia("spinner");
-    log.debug("hello world");
-    log.debug(options);
-    log.info(`root parent id: ${NOTION_ROOT_PARENT_ID}`);
-    kia.start();
-    await sleep(3000);
-    kia.succeed("finished");
-    const blocks = martian.markdownToBlocks(`
-## this is heading 2
-* [ ] todo
-`);
-    console.dir(blocks, { depth: Number.MAX_SAFE_INTEGER });
-  })
+  .example(example.title, example.contents)
+  .arguments("<command> [option]")
+  .command(motion.name, motion.desc)
+  .action(motion.action)
   .option("-d, --directory [directory:string]", "directory store markdown file")
   .command("help", new HelpCommand().global())
   .command("completions", new CompletionsCommand())
