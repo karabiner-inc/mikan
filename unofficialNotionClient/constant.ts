58f4ae13901c69c9361833d1f7a41b614390a275
Ichinose Yoshihiro <maruisansmai@gmail.com>
2022-02-22 15:25:08 +0900

    update: dotenv対応

--- unofficialNotionClient/constant.ts
+++ unofficialNotionClient/constant.ts
@@ -1,3 +1,4 @@
+import "https://deno.land/x/dotenv/load.ts";
 export const BASE_URL = "https://www.notion.so";
 export const API_BASE_URL = BASE_URL + "/api/v3";
 export const TOKEN_V2: string = Deno.env.get("TOKEN_V2") as string;
